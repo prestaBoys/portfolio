c731722ca015eeb0c20180ef0b8c117ffd68ece5
Francisco <franciscodagamac@gmail.com>
2022-07-21 11:44:41 +0800

    new push1

--- index.php
+++ index.php
@@ -3,10 +3,59 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title></title>
-	<link rel="stylesheet" href="">
+	<title>My Portfolio</title>
+	<link rel="icon" type="image/x-icon" href="assets/img/favicon.ico">
+	<link rel="stylesheet" href="assets/css/onepage-scroll.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-	<h1>Hello World!</h1>
+	
+
+	<main>
+		<header id="header" class=""></header>
+
+		<section class="main-wrapper">
+			<div class="section-1">
+				<div>
+					<img src="assets/img/user-logo.png" alt="Logo" class="img-logo">
+					<div class="social-wrapper">
+						<a href="" title="" target="_blank">
+							<lottie-player id="firstLottie" src="assets/img/linkedin/linkedin.json" style="width:50px; height: 50px;" hover></lottie-player>
+						</a>
+						<a href="" title="" target="_blank">
+							<lottie-player id="firstLottie" src="assets/img/instagram/instagram.json" style="width:50px; height: 50px;" hover></lottie-player>
+						</a>
+						<!-- <a href="" title="" target="_blank"><i class="fa fa-whatsapp" aria-hidden="true"></i></a> -->
+						<a href="" title="" target="_blank">
+							<lottie-player id="firstLottie" src="assets/img/mail/mail.json" style="width:50px; height: 50px;" hover></lottie-player>
+						</a>
+					</div>
+				</div>
+				<div>
+					<h1>I'm Moi</h1>
+					<p>An independent web developer specialized in <a href="" title="">Prestashop</a> and <a href="" title="">Wordpress</a>.
+					I used to code on my spare times, and mostly, I worked as a front end (HTML, JS, CSS3) and back end (Laravel, Codeigniter) as well. I work with both small and big companies from around the world as a full-time developer for the last 4 years that helps me polished my skills in development.</p>
+					<p>I'm currently employed but, you can still contact me if you want.</p>
+
+					<ul class="ul-list">
+						<li><i data-feather="briefcase"></i> Work</li>
+						<li><i data-feather="code"></i> Plugins</li>
+						<li><i data-feather="circle"></i> Hire me</li>
+					</ul>					
+				</div>
+			</div>
+		</section>
+	</main>
+
+
+	<script src="assets/js/jquery-3.6.0.min.js" type="text/javascript"></script>
+	
+	<script src="https://unpkg.com/@lottiefiles/lottie-player@0.4.0/dist/lottie-player.js"></script>
+
+	<script src="https://unpkg.com/feather-icons"></script>>
+
+	<script src="assets/js/jquery.onepage-scroll.min.js" type="text/javascript"></script>
+	<script src="assets/js/script.js" type="text/javascript"></script>
 </body>
 </html>
